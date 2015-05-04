77949154321fcc5447a5e86ea6581293d85a00dd
Sindoora90 <Sindoora@dhcppc17.im.lab.ifi.lmu.de>
2015-05-04 16:42:02 +0200

    delete fingerprints geht fast

--- app/src/main/java/com/example/sindoora/mspproject2/DistanceReasoner.java
+++ app/src/main/java/com/example/sindoora/mspproject2/DistanceReasoner.java
@@ -9,7 +9,11 @@ public class DistanceReasoner {
 
 	public Fingerprint compareClosest(ArrayList<Fingerprint> fingerprints, List<ScanResult> aps) {
 		//TODO: return closest match in fingerprint space
-
+        for (Fingerprint f: fingerprints){
+            for(ScanResult s: aps){
+               // f.measures.rss
+            }
+        }
         return null;
 	}
 }
