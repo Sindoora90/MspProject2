e2ac3e8d53e7e7b40beb17719b21ae4b9210da4a
kato-sama <lena.hunglinger@googlemail.com>
2015-05-04 13:42:34 +0200

    test push

--- app/src/main/java/com/example/sindoora/mspproject2/MainActivity.java
+++ app/src/main/java/com/example/sindoora/mspproject2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sindoora.mspproject2;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -45,6 +46,7 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+        Log.d("test", "test");
     }
 
 
